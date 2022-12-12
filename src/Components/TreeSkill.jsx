313f39c45a5d318d868e8fcf191c3fcbd9edab0d
Josipjosic <josip.josic1911@hotmail.com>
2022-12-12 15:41:25 +0100

    Added styles

--- src/Components/TreeSkill.jsx
+++ src/Components/TreeSkill.jsx
@@ -13,20 +13,22 @@ import mobx from "./assets/mobx.png";
 import sass from "./assets/sass.png";
 import mysql from "./assets/mySQL.png";
 import wordpress from "./assets/wordpress.png";
-import { useInView } from 'react-intersection-observer';
-
+import { useInView } from "react-intersection-observer";
 
 function TreeSkill() {
-
-  const { ref: myRef, inView: myElementIsVisible } = useInView({triggerOnce: true});
-
-
+  const { ref: myRef, inView: myElementIsVisible } = useInView({
+    triggerOnce: true,
+  });
 
   return (
-    <section className="tree" id="skills"  ref={myRef}>
+    <section className="tree" id="skills" ref={myRef}>
       <div className="container">
         <div className="text-center">
-          <h3 className={`${"fs-2"} ${myElementIsVisible ? "about-animate" : ""}`}>Details</h3>
+          <h3
+            className={`${"fs-2"} ${myElementIsVisible ? "about-animate" : ""}`}
+          >
+            Details
+          </h3>
           <br></br>
           <p className="fs-4 my-sm-3">
             more about tools, technologies and social media I’m currently using
@@ -36,51 +38,82 @@ function TreeSkill() {
           <div className="col my-lg-5">
             <h4 className="fs-3 py-3 my-3">Technologies</h4>
             <div className="p-5 img-cont">
-              <img src={react} alt="react-icon" className="img1"></img>
-              <img src={figma} alt="figma-icon" className="img2"></img>
-              <img src={firebase} alt="firebase-icon" className="img3"></img>
-              <img src={git} alt="git-icon" className="img4"></img>
-              <br></br>
-              <img src={material} alt="material-icon" className="img5"></img>
-              <img src={mobx} alt="mobx-icon" className="img6"></img>
-              <img src={sass} alt="sass-icon" className="img7"></img>
-              <img src={mysql} alt="mysql-icon" className="img8"></img>
-              <img src={wordpress} alt="wp-icon" className="img9"></img>
+              <div className="img-div">
+                <img src={react} alt="react-icon" className="img1"></img>React
+              </div>
+              <div className="img-div">
+                <img src={figma} alt="figma-icon" className="img2"></img>Figma
+              </div>
+              <div className="img-div">
+                <img src={firebase} alt="firebase-icon" className="img3"></img>Firebase
+              </div>
+              <div className="img-div">
+                <img src={git} alt="git-icon" className="img4"></img>Git
+              </div>
+              <div className="img-div">
+                <img src={material} alt="material-icon" className="img5"></img>MaterialUI
+              </div>
+              <div className="img-div">
+                <img src={mobx} alt="mobx-icon" className="img6"></img>Mobx
+              </div>
+              <div className="img-div">
+                <img src={sass} alt="sass-icon" className="img7"></img>Sass
+              </div>
+              <div className="img-div">
+                <img src={mysql} alt="mysql-icon" className="img8"></img>MySQL
+              </div>
+              <div className="img-div">
+                <img src={wordpress} alt="wp-icon" className="img9"></img>Wordpress
+              </div>
             </div>
           </div>
           <div className="col my-lg-5">
             <h4 className="fs-3 py-3 my-3">Contact</h4>
             <div className="p-5 img-cont">
-              <a
-                href="https://www.facebook.com/josic12"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src={facebook} alt="react-icon" className="img11"></img>
-              </a>
-              <a
-                href="https://www.linkedin.com/in/josip-josic-949198213/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src={linkedin} alt="linkedin-icon" className="img12"></img>
-              </a>
-              <a
-                href="https://github.com/Josipjosic"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src={github} alt="github-icon" className="img13"></img>
-              </a>
+              <div className="cont-div">
+                <a
+                  href="https://www.facebook.com/josic12"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={facebook} alt="react-icon" className="img11"></img>
+                Facebook</a>
+              </div>
+              <div className="cont-div">
+                <a
+                  href="https://www.linkedin.com/in/josip-josic-949198213/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src={linkedin}
+                    alt="linkedin-icon"
+                    className="img12"
+                  ></img>
+                LinkedIn</a>
+              </div>
+              <div className="cont-div">
+                {" "}
+                <a
+                  href="https://github.com/Josipjosic"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={github} alt="github-icon" className="img13"></img>
+                GitHub</a>
+              </div>
 
-              <img
-                src={gmail}
-                alt="gmail-icon"
-                className="img14"
-                onClick={() => {
-                  window.location = "mailto:jjosic23@gmail.com";
-                }}
-              ></img>
+              <div className="cont-div">
+                {" "}
+                <img
+                  src={gmail}
+                  alt="gmail-icon"
+                  className="img14"
+                  onClick={() => {
+                    window.location = "mailto:jjosic23@gmail.com";
+                  }}
+                ></img>Gmail
+              </div>
             </div>
           </div>
         </div>
